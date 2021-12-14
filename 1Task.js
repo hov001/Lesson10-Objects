811682cc08ba0e4da18c251e913e071f7e76f96a
Gayane25 <gayabennu@gmail.com>
2021-12-14 21:26:26 +0400

    feat: adding task and its explanation in comment

---
+++ 1Task.js
@@ -0,0 +1,4 @@
+let person = { name: 'Sarah', country: 'Nigeria', job: 'Developer' };
+//let name, country, job;
+const { name, country, job } = person;
+console.log(name); // destructuring an object for creating variables name, country, job
