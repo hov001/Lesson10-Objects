a3169c5b2ff612c55e3ef9948462931afe184112
Gayane25 <gayabennu@gmail.com>
2021-12-14 22:22:16 +0400

    feat:solved with object destructuring method

--- getVoteCount.js
+++ getVoteCount.js
@@ -1,3 +1,11 @@
 'strict mode';
 /*Given an object containing counts of both upvotes and downvotes, return what vote count should be
 displayed. This is calculated by subtracting the number of downvotes from upvotes.*/
+
+function getVoteCount(obj) {
+  let { upvotes, downvotes } = obj;
+  return upvotes - downvotes;
+}
+console.log(getVoteCount({ upvotes: 13, downvotes: 0 }));
+console.log(getVoteCount({ upvotes: 2, downvotes: 33 }));
+console.log(getVoteCount({ upvotes: 132, downvotes: 132 }));
