b2d9ecaed2aae60d0be6e901e6640bd2a7bb8e23
Gayane25 <gayabennu@gmail.com>
2021-12-14 22:00:23 +0400

    feat:solved with spread operator

--- takeArguments.js
+++ takeArguments.js
@@ -1,2 +1,10 @@
 'use strict';
 //How to take arguments in sum function?
+
+function sum(...args) {
+  return args.reduce((sum, current) => {
+    return sum + current;
+  });
+}
+console.log(sum(1, 2)); // prints 3
+console.log(sum(1, 2, 3)); // prints 6
