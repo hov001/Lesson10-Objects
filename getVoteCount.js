a176df7e7a743627c89e1212dd5cbe82b86be5f4
Gayane25 <gayabennu@gmail.com>
2021-12-14 22:20:15 +0400

    added strict mode and ticket in comment

---
+++ getVoteCount.js
@@ -0,0 +1,3 @@
+'strict mode';
+/*Given an object containing counts of both upvotes and downvotes, return what vote count should be
+displayed. This is calculated by subtracting the number of downvotes from upvotes.*/
