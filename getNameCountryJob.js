fc53e45c1c1fe8ccad9429e46db8b5ea0d4d8bdf
Gayane25 <gayabennu@gmail.com>
2021-12-14 21:44:01 +0400

    feat: added distructuring feature to code

--- getNameCountryJob.js
+++ getNameCountryJob.js
@@ -1,2 +1,8 @@
 //Get name, country and jod using destructuring
 'use strict';
+
+let person = { name: 'Sarah', country: 'Armenia', job: 'Developer' };
+let { name, country, job } = person;
+console.log(name); // "Sarah"
+console.log(country); // "Armenia"
+console.log(job); // "Developer"
