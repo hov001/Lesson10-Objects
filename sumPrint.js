573470803aa796fec539d1430497093b8ea7587b
Gayane25 <gayabennu@gmail.com>
2021-12-15 01:25:23 +0400

    feat:strict mode and ticket are added

---
+++ sumPrint.js
@@ -0,0 +1,7 @@
+'use strict';
+/*Create a function that takes infinite count of elements, operator and prints their sum. If there's no
+operator, then default should be + 
+printSum("*", 1, 2, 3); // 9
+printSum(1, 2, 3, 4, 5); // 15
+printSum("-", 1, 2, 3, 6, 7); // -17
+printSum("**", 2, 3, 2); // 64 */
