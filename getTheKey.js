626cfcb1f2cbf21b5902c8ee8d216b937253a5f7
Gayane25 <gayabennu@gmail.com>
2021-12-15 20:26:38 +0400

    feat:solved with for in method

--- getTheKey.js
+++ getTheKey.js
@@ -1,3 +1,38 @@
 'use strict';
 /*Write object.keys, object.values and object.entries methods using for..in*/
 
+let myObject = {
+  name: 'Gayane',
+  lastName: 'Babayan',
+  age: 26,
+};
+
+function getKeys(obj) {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(key);
+  }
+  return arr;
+}
+console.log(getKeys(myObject));
+
+function getValues(obj) {
+  let arr = [];
+  for (let key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
+}
+
+console.log(getValues(myObject));
+
+function getEntries(obj) {
+  let arr = [];
+  let j = 0;
+  for (let key in obj) {
+    arr.push(key, obj[key]);
+  }
+  return arr;
+}
+
+console.log(getEntries(myObject));
